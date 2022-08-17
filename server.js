78a7e8da6d6f28745de53eb9f882ab576a7f7d4b
LiangRVR <liangvillarrubia95@gmail.com>
2022-08-17 15:29:07 -0400

    chore: change schema to an ecoomerce schema

--- server.js
+++ server.js
@@ -4,14 +4,63 @@ const { graphqlHTTP } = require("express-graphql");
 
 const Schema = buildSchema(`
     type Query {
-        description: String,
-        price: Float
+        products: [Product]
+        orders: [Order]
+    }
+
+    type Product {
+      id: ID!
+      description: String!
+      reviews: [Review]
+      price: Float!
+    }
+
+    type Review {
+      rating: Int!
+      comment: String
+    }
+
+    type Order {
+      date: String!
+      subtotal: Float!
+      items: [OrderItem]
+    }
+
+    type OrderItem {
+      product: Product!
+      quantity: Int!
     }
 `);
 
 const root = {
-  description: "Red Show",
-  price: 42.2,
+  products: [
+    {
+      id: "blueShoe",
+      description: "Blue Shoe",
+      price: 25.3,
+    },
+    {
+      id: "redJeans",
+      description: "Red Jeans",
+      price: 42.1,
+    },
+  ],
+  orders: [
+    {
+      date: "2005-12-20",
+      subtotal: 67.4,
+      items: [
+        {
+          product: {
+            id: "blueShoe",
+            description: "Old Blue Shoe",
+            price: 26.3,
+          },
+          quantity: 3,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
