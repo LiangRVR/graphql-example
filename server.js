5a36f20460a3cf81e9fe581aeb0fdf8cc64c8687
LiangRVR <liangvillarrubia95@gmail.com>
2022-08-17 14:27:48 -0400

    feat: creat a simple graphql server with express

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+
+const Schema = buildSchema(`
+    type Query {
+        description: String,
+        price: Float
+    }
+`);
+
+const root = {
+  description: "Red Show",
+  price: 42.2,
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: Schema,
+    rootValue: root,
+  })
+);
+
+app.listen(3000, () => {
+  console.log("Running GraphQL server...");
+});
