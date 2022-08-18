cac6817d4f39c64b13a5a5ba725be4c8cca0f635
LiangRVR <liangvillarrubia95@gmail.com>
2022-08-18 11:25:32 -0400

    feat: add query resolves to schema

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
+const { query } = require("express");
 
 const typeArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 
 const schema = makeExecutableSchema({
   typeDefs: typeArray,
+  resolvers: {
+    Query: {
+      products: async (parent) => {
+        console.log("Getting products ...");
+        const product =  await Promise.resolve(parent.products)
+        return product;
+      },
+      orders: (parent) => {
+        console.log("Getting orders ...");
+        return parent.orders;
+      },
+    },
+  },
 });
 
 const root = {
