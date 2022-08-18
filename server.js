9dadc850ea93bb6fb7e570666e086bd3296f3660
LiangRVR <liangvillarrubia95@gmail.com>
2022-08-18 15:24:07 -0400

    refactor: use apollo as a server for graphql

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
 const path = require("node:path");
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const { ApolloServer } = require("apollo-server-express");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
-const { query } = require("express");
 
 const typeArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-const schema = makeExecutableSchema({
-  typeDefs: typeArray,
-  resolvers: resolversArray,
-});
+const startApolloServer = async () => {
+  const schema = makeExecutableSchema({
+    typeDefs: typeArray,
+    resolvers: resolversArray,
+  });
 
-const app = express();
+  const app = express();
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-  })
-);
+  const server = new ApolloServer({
+    schema,
+  });
 
-app.listen(3000, () => {
-  console.log("Running GraphQL server...");
-});
+  await server.start();
+
+  server.applyMiddleware({
+    app,
+    path: "/graphql",
+  });
+
+  app.listen(3000, () => {
+    console.log("Running GraphQL server...");
+  });
+};
+
+startApolloServer();
